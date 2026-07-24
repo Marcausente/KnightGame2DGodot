f56541efd0d45ddf976b6b08242408e7dc742f07
Marc Fernández <marcausente@gmail.com>
2026-07-25 01:08:20 +0200

    Refactor: Reordenado el codigo en funciones para el movimiento horizontal

--- Escenas/personaje/personaje.gd
+++ Escenas/personaje/personaje.gd
@@ -5,15 +5,20 @@ var _speed: float = 65.00
 func _ready(): #Al iniciar el script se ejecuta esta funcion
 	inicio_funcion_test()
 
-func _process(delta): #Es una funcion que se llama constantemente, una vez cada frame, y recibe delta que es el numero en decimales del frame anterior
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = _speed
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -_speed
-	else: 
-		velocity.x = 0
-	move_and_slide() #Esta funcion se debe poner en las funciones de movimiento, hace que se gestionen solas las colisiones
+func _physics_process(delta): #Es una funcion que se llama constantemente, una vez cada frame, y recibe delta que es el numero en decimales del frame anterior
+	
+	movimiento_horizontal(_speed) #Ejecuta la funcion de movimiento horizontal
+	move_and_slide()
+
 
 func inicio_funcion_test(): #Funcion que nos da los datos de prueba por consola
 	print("Se ha iniciado la funcion")
 	print("Velocidad: ", _speed) 
+	
+func movimiento_horizontal(speed):
+	if Input.is_action_pressed("ui_right"): #Al apretarse la flecha derecha
+		velocity.x = speed #La velocidad se incrementa en el eje X (Derecha)
+	elif Input.is_action_pressed("ui_left"): #Al apretarse la flecha Izquierda
+		velocity.x = -speed #La velocidad se resta en el eje X (Izquierda)
+	else: 
+		velocity.x = 0
