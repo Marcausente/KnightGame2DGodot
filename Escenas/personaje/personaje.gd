6f6980475fda49d4a640bf86695ea2023ecabb16
Marc Fernández <marcausente@gmail.com>
2026-07-25 01:03:12 +0200

    FEAT: cambiada velocidad a variable para mayor orden y subida

--- Escenas/personaje/personaje.gd
+++ Escenas/personaje/personaje.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
-var speed: float = 70.00
+var _speed: float = 65.00
 
 func _ready(): #Al iniciar el script se ejecuta esta funcion
 	inicio_funcion_test()
 
 func _process(delta): #Es una funcion que se llama constantemente, una vez cada frame, y recibe delta que es el numero en decimales del frame anterior
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
+		velocity.x = _speed
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50
+		velocity.x = -_speed
 	else: 
 		velocity.x = 0
-	move_and_slide()
+	move_and_slide() #Esta funcion se debe poner en las funciones de movimiento, hace que se gestionen solas las colisiones
 
 func inicio_funcion_test(): #Funcion que nos da los datos de prueba por consola
 	print("Se ha iniciado la funcion")
-	print("Velocidad: ", speed) 
+	print("Velocidad: ", _speed) 
