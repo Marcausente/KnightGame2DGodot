b3c15943db9180637bfb2ac96b7ee6bef15c456d
Marc Fernández <marcausente@gmail.com>
2026-07-24 20:19:36 +0200

    DOC: Añadidos comentarios de codigo en scrpt de personaje

--- Escenas/personaje/personaje.gd
+++ Escenas/personaje/personaje.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-func _ready():
+var speed: float = 100.00
+
+func _ready(): #Al iniciar el script se ejecuta esta funcion
 	inicio_funcion_test()
 
 
-func inicio_funcion_test():
+func inicio_funcion_test(): #Funcion que nos da los datos de prueba por consola
 	print("Se ha iniciado la funcion")
+	print(speed)
