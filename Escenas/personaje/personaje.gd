8a239d106d5689dad21aa9fde50394b9cbdbe01f
Marc Fernández <marcausente@gmail.com>
2026-07-25 01:29:30 +0200

    FIX: Arreglado algunos errores menores en el salto al momento de pasar las variables

--- Escenas/personaje/personaje.gd
+++ Escenas/personaje/personaje.gd
@@ -10,7 +10,7 @@ func _ready(): #Al iniciar el script se ejecuta esta funcion
 func _physics_process(delta): #Es una funcion que se llama constantemente, una vez cada frame, y recibe delta que es el numero en decimales del frame anterior
 	
 	movimiento_horizontal(_speed) #Ejecuta la funcion de movimiento horizontal
-	movimiento_vertical(delta, _jump)
+	movimiento_vertical(delta)
 	move_and_slide() #Basciamente hace todas las comprobaciones de fisica con paredes, suelos etc despues de mover
 
 
@@ -26,11 +26,11 @@ func movimiento_horizontal(speed):
 	else: 
 		velocity.x = 0
 
-func movimiento_vertical(delta, jump):
+func movimiento_vertical(delta):
 	if is_on_floor():
-		contador_salto = 2
+		contador_salto = 2 #Cuando esta en el suelo, la varibale de contador de salto vuelve a 2
 	velocity += get_gravity() * delta #Lo mismo que poner velocity = velocity + get_gravity(), le sumas el valor de la gravedad a la variable velocity
-	if Input.is_action_just_pressed("ui_accept") and contador_salto > 0:
-		velocity.y = jump
-		contador_salto = contador_salto-1
+	if Input.is_action_just_pressed("ui_accept") and contador_salto > 0: #Si presionas el espacio y hay mas de 0 contadores de salto
+		velocity.y = _jump #Aumentara en el eje y la velocidad de salto
+		contador_salto = contador_salto-1 #Te quita un contador de salto, para que solo tengas un salto extra
 		print(contador_salto)
