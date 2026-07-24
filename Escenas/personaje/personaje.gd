be01d66106536c5a18fe5e6cbac7e416b6298281
Marc Fernández <marcausente@gmail.com>
2026-07-25 01:01:03 +0200

    FEAT: Movimientos laterales del personaje

--- Escenas/personaje/personaje.gd
+++ Escenas/personaje/personaje.gd
@@ -1,13 +1,18 @@
 extends CharacterBody2D
 
-var speed: float = 100.00
+var speed: float = 70.00
 
 func _ready(): #Al iniciar el script se ejecuta esta funcion
 	inicio_funcion_test()
 
 func _process(delta): #Es una funcion que se llama constantemente, una vez cada frame, y recibe delta que es el numero en decimales del frame anterior
-	position.x += 10 * delta
-
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = 50
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -50
+	else: 
+		velocity.x = 0
+	move_and_slide()
 
 func inicio_funcion_test(): #Funcion que nos da los datos de prueba por consola
 	print("Se ha iniciado la funcion")
