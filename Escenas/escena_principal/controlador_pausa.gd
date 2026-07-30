4e12d461d7ce14594edd0ef5bddfc3c658f0fcc0
Marc Fernández <marcausente@gmail.com>
2026-07-30 17:05:36 +0200

    FEAT: Fuente de letra cambiada en el texto de la pausa

--- Escenas/escena_principal/controlador_pausa.gd
+++ Escenas/escena_principal/controlador_pausa.gd
@@ -5,7 +5,7 @@ extends Control
 func _input(event: InputEvent) -> void: #Esta funcion recibe un evento input, es decir, que se apriete algo
 	if event.is_action_pressed("pausa") and get_tree().paused == false: #Si se aprieta el boton que tenemos configurado para la pausa
 		get_tree().paused = true #Pone la pausa
-		texto_pausa.visible = true
+		texto_pausa.visible = true #Se muestra el texto de texto_pausa
 	elif event.is_action_pressed("pausa") and get_tree().paused == true: #Si se aprieta el boton de pausa Y ya estaba pausado
 		get_tree().paused = false #Quita la pausa
-		texto_pausa.visible = false
+		texto_pausa.visible = false #Se quita el texto de texto_pausa
