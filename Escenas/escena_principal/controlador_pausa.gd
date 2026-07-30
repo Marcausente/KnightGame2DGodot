cea9e41005fabc90edeccf17b3f9c798689ec89c
Marc Fernández <marcausente@gmail.com>
2026-07-30 17:04:16 +0200

    FEAT: Implementado texto de pausa

--- Escenas/escena_principal/controlador_pausa.gd
+++ Escenas/escena_principal/controlador_pausa.gd
@@ -1,7 +1,11 @@
 extends Control
 
+@export var texto_pausa: Control
+
 func _input(event: InputEvent) -> void: #Esta funcion recibe un evento input, es decir, que se apriete algo
 	if event.is_action_pressed("pausa") and get_tree().paused == false: #Si se aprieta el boton que tenemos configurado para la pausa
-		get_tree().paused = true
-	elif event.is_action_pressed("pausa") and get_tree().paused == true:
-		get_tree().paused = false
+		get_tree().paused = true #Pone la pausa
+		texto_pausa.visible = true
+	elif event.is_action_pressed("pausa") and get_tree().paused == true: #Si se aprieta el boton de pausa Y ya estaba pausado
+		get_tree().paused = false #Quita la pausa
+		texto_pausa.visible = false
